8cc65ece32d8981c68915cc99f346eb71e2274d2
Giovanni Granzotti <giogranzotti@hotmail.com>
2020-09-15 21:38:14 -0300

    Criados atributos de instâncias nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=25):
+        self.idade = idade
+        self.nome = nome
+
     def comprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    P = Pessoa()
+    P = Pessoa('Luciano')
     print(Pessoa.comprimentar(P))
     print(id(P))
     print(P.comprimentar())
+    print(P.nome)
+    P.nome = 'Giovanni'
+    print(P.nome)
+    print(P.idede)
