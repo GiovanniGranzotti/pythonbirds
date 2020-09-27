67495f0450ef298d0c49bd9dcdb374e1e208a214
Giovanni <giogranzotti@hotmail.com>
2020-09-27 14:25:21 -0300

    Aprendendo a usar unittest para testar o programa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,9 +17,14 @@ class Pessoa:
     def nome_e_atributo_de_classe(cls):
         return f'{cls} - olhos: {cls.olhos}'
 
+
+class Homem(Pessoa):
+    pass
+
+
 if __name__ == '__main__':
-    Giovanni = Pessoa(nome='Giovanni')
-    Ademir = Pessoa(Giovanni, nome='Ademir')
+    Giovanni = Homem(nome='Giovanni')
+    Ademir = Homem(Giovanni, nome='Ademir')
     print(Pessoa.comprimentar(Ademir))
     print(id(Ademir))
     print(Ademir.comprimentar())
@@ -40,3 +45,8 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(Giovanni.olhos), id(Ademir.olhos))
     print(Pessoa.metodo_estatico(), Ademir.metodo_estatico())
     print(Pessoa.nome_e_atributo_de_classe(), Ademir.nome_e_atributo_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(Giovanni, Pessoa))
+    print(isinstance(Giovanni, Homem))
