3d876c66c813fe54b7b50d21315a24eb9ccdaa3c
Giovanni Granzotti <giogranzotti@hotmail.com>
2020-10-16 21:56:07 -0300

    Término do pythonbirds

--- atores.py
+++ atores.py
@@ -9,14 +9,14 @@ ATIVO = 'Ativo'
 GRAVIDADE = 10  # m/s^2
 
 
-class Ator():
+class Ator:
     """
     Classe que representa um ator. Ele representa um ponto cartesiano na tela.
     """
     _caracter_ativo = 'A'
     _caracter_destruido = ' '
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x = 0, y = 0):
         """
         Método de inicialização da classe. Deve inicializar os parâmetros x, y, caracter e status
 
@@ -33,14 +33,14 @@ class Ator():
     def calcular_posicao(self, tempo):
         """
         Método que calcula a posição do ator em determinado tempo.
-        Deve-se imaginar que o tempo começa em 0 e avança de 0,01 segundos
+        Deve-se imaginar que o tempo começa em 0 e avança de 0, 01 segundos
 
         :param tempo: o tempo do jogo
         :return: posição x, y do ator
         """
-        return 1, 1
+        return self.x, self.y
 
-    def colidir(self, outro_ator, intervalo=1):
+    def colidir(self, outro_ator, intervalo = 1):
         """
         Método que executa lógica de colisão entre dois atores.
         Só deve haver colisão se os dois atores tiverem seus status ativos.
@@ -52,7 +52,15 @@ class Ator():
         :param intervalo: Intervalo a ser considerado
         :return:
         """
-        pass
+        if self.status == ATIVO and outro_ator.status == ATIVO:
+            # A diferença entre os pontos determina a distancia entre eles.
+            delta_x = abs(self.x - outro_ator.x)
+            delta_y = abs(self.y - outro_ator.y)
+            if delta_x <= intervalo and delta_y <= intervalo:
+                self.status = outro_ator.status = DESTRUIDO
+
+
+
 
 
 
@@ -73,7 +81,7 @@ class DuploLancamentoExcecao(Exception):
 class Passaro(Ator):
     velocidade_escalar = 10
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x = 0, y = 0):
         """
         Método de inicialização de pássaro.
 
@@ -95,15 +103,16 @@ class Passaro(Ator):
 
         :return: booleano
         """
-        return True
+        return not self._tempo_de_lancamento is None
 
     def colidir_com_chao(self):
         """
-        Método que executa lógica de colisão com o chão. Toda vez que y for menor ou igual a 0,
+        Método que executa lógica de colisão com o chão. Toda vez que y for menor ou igual a 0, 
         o status dos Passaro deve ser alterado para destruido, bem como o seu caracter
 
         """
-        pass
+        if self.y <= 0:
+            self.status = DESTRUIDO
 
     def calcular_posicao(self, tempo):
         """
@@ -111,7 +120,7 @@ class Passaro(Ator):
 
         Antes do lançamento o pássaro deve retornar o valor de sua posição inicial
 
-        Depois do lançamento o pássaro deve calcular de acordo com sua posição inicial, velocidade escalar,
+        Depois do lançamento o pássaro deve calcular de acordo com sua posição inicial, velocidade escalar, 
         ângulo de lancamento, gravidade (constante GRAVIDADE) e o tempo do jogo.
 
         Após a colisão, ou seja, ter seus status destruido, o pássaro deve apenas retornar a última posição calculada.
@@ -119,8 +128,12 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return 1, 1
+        if self._esta_voando():
+            delta_t = tempo - self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+            self._calcular_posicao_horizontal(delta_t)
 
+        return super().calcular_posicao(tempo)
 
     def lancar(self, angulo, tempo_de_lancamento):
         """
@@ -131,14 +144,33 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
-        pass
+        self._angulo_de_lancamento = math.radians(angulo)
+        self._tempo_de_lancamento = tempo_de_lancamento
+    
+    def _calcular_posicao_vertical(self, delta_t):
+        y_atual = self._y_inicial
+        angulo_radianos = self._angulo_de_lancamento
+        y_atual += self.velocidade_escalar * math.sin(angulo_radianos) * delta_t
+        y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
+        self.y = y_atual
 
+    def _calcular_posicao_horizontal(self, delta_t):
+        x_atual = self._x_inicial
+        angulo_radianos = self._angulo_de_lancamento
+        x_atual += self.velocidade_escalar * math.cos(angulo_radianos) * delta_t
+        self.x = x_atual
+
+    def _esta_voando(self):
+        return self.foi_lancado() and self.status == ATIVO
 
 class PassaroAmarelo(Passaro):
     _caracter_ativo = 'A'
     _caracter_destruido = 'a'
+    velocidade_escalar = 30
 
 
 class PassaroVermelho(Passaro):
     _caracter_ativo = 'V'
     _caracter_destruido = 'v'
+    velocidade_escalar = 20
+# %%
